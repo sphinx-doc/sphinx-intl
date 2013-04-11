3153d9fdce10dd6c3e6cde9d7d3eb3558389a9df
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2013-04-11 10:07:44 +0900

    Remove python 2.4. This version has never been supported.

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
         "Topic :: Utilities",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.4",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
