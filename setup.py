2b5b225598916c7f5d9953573bd1789ceab18617
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2014-07-10 08:07:35 +0900

    0.9.5 release

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     author="Takayuki SHIMIZUKAWA",
     author_email="shimizukawa@gmail.com",
