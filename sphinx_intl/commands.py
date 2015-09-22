0462baa79b4cc1fba4c5f46ea0588ea36b658a2f
shimizukawa <shimizukawa@gmail.com>
2015-09-22 10:27:08 +0900

    fix levenshtein function to fit to py3.

--- sphinx_intl/commands.py
+++ sphinx_intl/commands.py
@@ -226,10 +226,10 @@ def get_tx_root():
 # 4th version, seems short and fast enough compared to the others.
 def levenshtein(seq1, seq2):
     oneago = None
-    thisrow = range(1, len(seq2) + 1) + [0]
-    for x in xrange(len(seq1)):
+    thisrow = list(range(1, len(seq2) + 1)) + [0]
+    for x in range(len(seq1)):
         twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
-        for y in xrange(len(seq2)):
+        for y in range(len(seq2)):
             delcost = oneago[y] + 1
             addcost = thisrow[y - 1] + 1
             subcost = oneago[y - 1] + (seq1[x] != seq2[y])
