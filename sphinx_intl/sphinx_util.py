6beb36fcb50f28671ae70af9f6ab24431883cb11
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-04-15 23:04:01 +0900

    Fix flake8 violations

--- sphinx_intl/sphinx_util.py
+++ sphinx_intl/sphinx_util.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 
+if False:
+    from typing import Iterator, List
+
 
 # port from https://github.com/sphinx-doc/sphinx/blob/ad41e0b/sphinx/util/tags.py
 class Tags(object):
     def __init__(self, tags=None):
-        # type: (List[unicode]) -> None
+        # type: (List[str]) -> None
         self.tags = dict.fromkeys(tags or [], True)
 
     def has(self, tag):
-        # type: (unicode) -> bool
+        # type: (str) -> bool
         return tag in self.tags
 
     __contains__ = has
 
     def __iter__(self):
-        # type: () -> Iterator[unicode]
+        # type: () -> Iterator[str]
         return iter(self.tags)
 
     def add(self, tag):
-        # type: (unicode) -> None
+        # type: (str) -> None
         self.tags[tag] = True
 
     def remove(self, tag):
-        # type: (unicode) -> None
+        # type: (str) -> None
         self.tags.pop(tag, None)
