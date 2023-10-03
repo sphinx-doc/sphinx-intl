463f65fe3f4c6447d656524906bcf3c679121b6e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-10-03 14:04:41 -0400

    Set resource_name in .tx/config files to preserve resource naming behavior prior to 2.1.0 (#90)

--- tests/test_cmd_transifex.py
+++ tests/test_cmd_transifex.py
@@ -73,6 +73,7 @@ def test_update_txconfig_resources_with_config(home_in_temp, temp):
 
     data = (tx_dir / 'config').text()
     assert re.search(r'\[o:eggs-org:p:ham-project:r:README\]', data)
+    assert re.search(r'\nresource_name = README\n', data)
 
 
 def test_update_txconfig_resources_with_pot_dir_argument(home_in_temp, temp):
@@ -95,6 +96,7 @@ def test_update_txconfig_resources_with_pot_dir_argument(home_in_temp, temp):
 
     data = (tx_dir / 'config').text().replace('\\', '/')
     assert re.search(r'\[o:eggs-org:p:ham-project:r:README\]', data)
+    assert re.search(r'\nresource_name = README\n', data)
     assert re.search(r'source_file\W*=\W*_build/locale/README.pot', data)
 
 
@@ -118,6 +120,7 @@ def test_update_txconfig_resources_with_project_name_including_dots(home_in_temp
 
     data = (tx_dir / 'config').text()
     assert re.search(r'\[o:eggs-org:p:ham-projectcom:r:README\]', data)
+    assert re.search(r'\nresource_name = README\n', data)
 
 
 def test_update_txconfig_resources_with_project_name_including_spaces(home_in_temp, temp):
@@ -140,6 +143,7 @@ def test_update_txconfig_resources_with_project_name_including_spaces(home_in_te
 
     data = (tx_dir / 'config').text()
     assert re.search(r'\[o:eggs-org:p:ham-project-com:r:README\]', data)
+    assert re.search(r'\nresource_name = README\n', data)
 
 
 def test_update_txconfig_resources_with_potfile_including_symbols(home_in_temp, temp):
@@ -170,3 +174,33 @@ def test_update_txconfig_resources_with_potfile_including_symbols(home_in_temp, 
     data = (tx_dir / 'config').text()
     assert re.search(r'\[o:eggs-org:p:ham-project-com:r:example_document\]', data)
     assert re.search(r'\[o:eggs-org:p:ham-project-com:r:test_document\]', data)
+    assert re.search(r'\nresource_name = example_document\n', data)
+    assert re.search(r'\nresource_name = test_document\n', data)
+
+
+def test_update_txconfig_resources_with_potfile_including_path_separators(home_in_temp, temp):
+    tx_dir = temp / '.tx'
+    tx_dir.makedirs()
+    (tx_dir / 'config').write_text(dedent("""\
+    [main]
+    host = https://www.transifex.com
+    """))
+
+    (temp / '_build' / 'locale').copytree(temp / 'locale' / 'pot')
+
+    # copy README.pot to 'example document.pot'
+    readme = (temp / '_build' / 'locale' / 'README.pot').text()
+    (temp / '_build' / 'locale' / 'example').makedirs()
+    (temp / '_build' / 'locale' / 'example' / 'document.pot').write_text(readme)
+
+    r1 = runner.invoke(commands.main,
+                       ['update-txconfig-resources',
+                        '-d', 'locale',
+                        '--transifex-organization-name', 'eggs-org',
+                        '--transifex-project-name', 'ham project com',
+                        ])
+    assert r1.exit_code == 0
+
+    data = (tx_dir / 'config').text()
+    assert re.search(r'\[o:eggs-org:p:ham-project-com:r:example--document\]', data)
+    assert re.search(r'\nresource_name = example--document\n', data)
