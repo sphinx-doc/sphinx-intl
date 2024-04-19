ef259ffc4cab9cfc1501bbac0625c93145c33fe8
James Knight <james.d.knight@live.com>
2024-04-19 16:14:22 -0400

    Support no obsolete option (#65)
    * tests: Sanity check line-width option
    
    Provide a unit test to validate the usage of the `--line-width` option.
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    
    * Adjust catalog commands to support forwarding options to babel
    
    The following tweaks the catalog wrapper commands used to call babel's
    `pofile`/`mofile` functions to support forwarding any keyword argument.
    This can provide flexibility to callers which may wish to utilize
    additional options provided by babel, without needing to add an explicit
    option in the catalog wrapper calls.
    
    A compatibility adjustment was added to `dump_po` to help map an
    original argument `line_width` to babel's `width` argument. This is to
    ensure callers which still use the `line_width` argument can
    appropriately configure the babel call to impose the provided width.
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    
    * Correct docstring spelling
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    
    * Provide no-obsolete option
    
    Adds support for a `--no-obsolete` option to allow the automatic
    exclusion of obsolete messages (`#~ `) from updated sources. This
    utilizes babel's `ignore_obsolete` option to not produce obsolete in a
    final pot file.
    
    The `--no-obsolete` argument was chosen to mimic the same argument
    naming as seen in `msgattrib` [1].
    
    [1]: https://www.gnu.org/software/gettext/manual/html_node/msgattrib-Invocation.html
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    
    * tests: Validate newly added no-obsolete option
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    
    ---------
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/test_cmd_pot.py
+++ tests/test_cmd_pot.py
@@ -8,6 +8,9 @@
     :copyright: Copyright 2019 by Takayuki SHIMIZUKAWA.
     :license: BSD, see LICENSE for details.
 """
+
+import os
+
 from click.testing import CliRunner
 
 from sphinx_intl import commands
@@ -65,6 +68,83 @@ def test_update_difference_detect(temp):
     assert r4.output.count('Create:') == 0
     assert r4.output.count('Update:') == 0
     assert r4.output.count('Not Changed:') == 1
+
+
+def test_update_line_width(temp):
+    with open('_build/locale/README.pot', 'r') as f:
+        template = f.read()
+
+    with open('_build/locale/README.pot', 'w') as f:
+        f.write(template)
+        f.write('\nmsgid "foorbar identifier1"\nmsgstr ""\n')
+
+    po_dir = os.path.join('locale', 'ja', 'LC_MESSAGES')
+    po_file = os.path.join(po_dir, 'README.po')
+
+    r1 = runner.invoke(commands.update, ['-d', 'locale', '-p', '_build/locale', '-l', 'ja'])
+    assert r1.exit_code == 0
+
+    with open(po_file, 'r') as f:
+        contents = f.read()
+        assert '"foorbar identifier1"\n' in contents
+
+    # change the identifier to trigger an update and impose a lower line-width count
+    with open('_build/locale/README.pot', 'w') as f:
+        f.write(template)
+        f.write('\nmsgid "foorbar identifier2"\nmsgstr ""\n')
+
+    r2 = runner.invoke(commands.update, ['-d', 'locale', '-p', '_build/locale', '-w', '1'])
+    assert r2.exit_code == 0
+
+    with open(po_file, 'r') as f:
+        contents = f.read()
+        assert '"foorbar"\n' in contents
+        assert '"identifier2"\n' in contents
+
+
+def test_update_no_obsolete(temp):
+    with open('_build/locale/README.pot', 'r') as f:
+        template = f.read()
+
+    with open('_build/locale/README.pot', 'w') as f:
+        f.write(template)
+        f.write('\nmsgid "foorbar1"\nmsgstr ""\n')
+        f.write('\nmsgid "foorbar2"\nmsgstr ""\n')
+
+    po_dir = os.path.join('locale', 'ja', 'LC_MESSAGES')
+    po_file = os.path.join(po_dir, 'README.po')
+
+    r1 = runner.invoke(commands.update, ['-d', 'locale', '-p', '_build/locale', '-l', 'ja'])
+    assert r1.exit_code == 0
+
+    with open(po_file, 'r') as f:
+        contents = f.read()
+        assert '\nmsgid "foorbar1"\n' in contents
+        assert '\nmsgid "foorbar2"\n' in contents
+
+    # remove the foorbar2 and verify we can see the obsolete entry
+    with open('_build/locale/README.pot', 'w') as f:
+        f.write(template)
+        f.write('\nmsgid "foorbar1"\nmsgstr ""\n')
+
+    r2 = runner.invoke(commands.update, ['-d', 'locale', '-p', '_build/locale'])
+    assert r2.exit_code == 0
+
+    with open(po_file, 'r') as f:
+        contents = f.read()
+        assert '\n#~ msgid "foorbar2"\n' in contents
+
+    # remove the foorbar1 and verify we can no longer see any obsolete entry
+    with open('_build/locale/README.pot', 'w') as f:
+        f.write(template)
+
+    r3 = runner.invoke(commands.update, ['-d', 'locale', '-p', '_build/locale', '--no-obsolete'])
+    assert r3.exit_code == 0
+
+    with open(po_file, 'r') as f:
+        contents = f.read()
+        assert 'msgid "foorbar1"' not in contents
+        assert 'msgid "foorbar2"' not in contents
 
 
 def test_stat(temp):
